e5986405300cbcc3b0d4feb32c9c993defce56d0
dorairaj98 <69347899+dorairaj98@users.noreply.github.com>
2020-10-24 19:15:47 +0530

    Update settings.py

--- ticket_webapp/ticket_webapp/settings.py
+++ ticket_webapp/ticket_webapp/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'ticket_webapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'dorairaj$default',
+        'USER': 'dorairaj',
+        'PASSWORD': 'RaJ@43#$',
+        'HOST': 'dorairaj.mysql.pythonanywhere-services.com',
     }
 }
 
