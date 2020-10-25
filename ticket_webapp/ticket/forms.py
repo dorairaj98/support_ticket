646582010d29dbedf7260c22eab91ae882ff9d91
dorairaj98 <69347899+dorairaj98@users.noreply.github.com>
2020-10-26 02:14:51 +0530

    Add files via upload

--- ticket_webapp/ticket/forms.py
+++ ticket_webapp/ticket/forms.py
@@ -4,10 +4,14 @@ from django.contrib.auth.forms import UserCreationForm
 
 
 class UserRegisterForm(UserCreationForm):
+
     email = forms.EmailField()
+    first_name = forms.CharField(max_length=20)
+    last_name = forms.CharField(max_length=20)
+
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        fields = ['username','first_name','last_name', 'email', 'password1', 'password2',]
         help_texts = {
             'username': None,
         }
--- ticket_webapp/ticket/views.py
+++ ticket_webapp/ticket/views.py
@@ -6,7 +6,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import AuthenticationForm
 from .forms import UserRegisterForm
 import requests
-
+import json
 
 def register(request):
     if request.method == 'POST':
@@ -18,7 +18,6 @@ def register(request):
     else:
         form = UserRegisterForm()
     return render(request, 'ticket/signup.html', {'form': form, 'title': 'reqister here'})
-
 
 def Login(request):
     if request.method == 'POST':
@@ -32,35 +31,141 @@ def Login(request):
         else:
             messages.info(request, f'account done not exit plz sign in')
     form = AuthenticationForm()
-    return render(request, 'ticket/login.html', {'form': form, 'title': 'log in'})
+    return render(request, 'ticket/login.html', {'form': form, 'title': 'login'})
 
 @login_required(login_url='/login/')
 def dashboard(request):
+    global ticketNumber, status, subject, description
     u = User.objects.get(id=request.user.id)
-    return render(request, 'ticket/dashboard.html', {"name": u})
+    ticketid = u.profile.ticket_support_id
+
+    auth_token = "9446933330c7f886fbdf16782906a9e0"
+    org_id = "60001280952"
+
+    ticket_id = str(ticketid)
+    params = "include=contacts,products"
+
+    headers = {
+        "Authorization": auth_token,
+        "orgId": org_id,
+        "contentType": "application/json; charset=utf-8"
+    }
+
+    ticket_request = requests.get('https://desk.zoho.in/api/v1/tickets/' + ticket_id + '?' + params, headers=headers)
+
+    if ticket_id:
+        if ticket_request.status_code == 200:
+            print("Request Successful,Response:")
+            a = json.loads(ticket_request.content)
+            ticketNumber = a["ticketNumber"]
+            status = a["status"]
+            subject = a["subject"]
+            description = a["description"]
+            print(a["ticketNumber"])
+            print(a["status"])
+            print(a["subject"])
+            print(a["description"])
+        else:
+            print("Request not successful,Response code ", ticket_request.status_code, " \nResponse : ",ticket_request.content)
+            ticketNumber = "TicketNumber"
+            status = "status"
+            subject = "subject"
+            description = "description"
+
+    check_id = u.profile.customer_id
+    if check_id is not None:
+        return render(request, 'ticket/dashboard.html', {"name": u,"ticketNumber":ticketNumber,"status":status, "subject": subject, "description":description})
+    else:
+        contact_data = {
+            "lastName": u.last_name,
+            "firstName": u.first_name,
+            "email": u.email,
+        }
+
+        headers = {
+            "Authorization": auth_token,
+            "orgId": org_id,
+            "contentType": "application/json; charset=utf-8"
+        }
+        request_api = requests.post('https://desk.zoho.in/api/v1/contacts', headers=headers,data=json.dumps(contact_data))
+
+        if request_api.status_code == 200:
+            print("Request Successful,Response:")
+            a = json.loads(request_api.content)
+            print(a['id'])
+            u.profile.customer_id = a['id']
+            u.profile.save()
+        else:
+            print("Request not successful,Response code ", request_api.status_code, " \nResponse : ",request_api.content)
+        return render(request, 'ticket/dashboard.html', {"name": u,"ticketNumber":ticketNumber,"status":status, "subject": subject, "description":description})
 
 @login_required(login_url='/login/')
 def submit_ticket(request):
+    global department,a
+    department = []
+    id =[]
+    category = ["-None-","NEW Project CI/CD Pipeline Setup","Update CI/CD Pipeline Configuration","DevSecOps Pipeline Setup","CI/CD pipeline failure","others"]
+    auth_token = "9446933330c7f886fbdf16782906a9e0"
+    org_id = "60001280952"
+
+    headers = {
+        "Authorization": auth_token,
+        "orgId": org_id,
+        "contentType": "application/json; charset=utf-8"
+    }
+    request_api = requests.get('https://desk.zoho.in/api/v1/departments', headers=headers)
+
+    if request_api.status_code == 200:
+        print("Request Successful,Response:")
+        a = json.loads(request_api.content)
+        for i in a['data']:
+            department.append(i['name'])
+            id.append(i['id'])
+    else:
+        print("Request not successful,Response code ", request_api.status_code, " \nResponse : ", request_api.content)
+
     u = User.objects.get(id=request.user.id)
     if "GET" == request.method:
-        return render(request, "ticket/forms.html",{"name": u,"email":u.email})
+        return render(request, "ticket/forms.html",{"name": u,"email":u.email,"department":department,"category":category})
     if request.method == 'POST':
         u = User.objects.get(id=request.user.id)
+        Contact_id = u.profile.customer_id
         Name = u,
         Email = u.email
-        Department= request.POST.get("department")
+        Department_name= request.POST.get("department")
         category = request.POST.get("category")
         subject = request.POST.get("subject")
         description = request.POST.get("description")
         priority = request.POST.get("priority")
 
-        Details = {'Name' : Name,
-             'Email' :Email,
-             'Department': Department,
-             'category': category,
-             'subject':subject,
-             'description':description,
-             'priority':priority}
-        print(Details)
-        messages.success(request, f' Ticket Submitted')
+        Department = [d['id'] for d in a['data'] if d['name'] == Department_name]
+
+        ticket_data = {
+            "email" : Email,
+            "departmentId": "".join(Department),
+            "contactId": Contact_id,
+            "category": category,
+            "subject": subject,
+            "description" : description,
+            "priority" : priority,
+        }
+
+        headers = {
+            "Authorization": auth_token,
+            "orgId": org_id,
+            "contentType": "application/json; charset=utf-8"
+        }
+
+        ticekt_request = requests.post('https://desk.zoho.in/api/v1/tickets', headers=headers,data=json.dumps(ticket_data))
+
+        if ticekt_request.status_code == 200:
+            print("Request Successful,Response:")
+            a = json.loads(ticekt_request.content)
+            u.profile.ticket_support_id = a["id"]
+            u.profile.save()
+            print(a)
+            messages.success(request, f' Ticket is Created')
+        else:
+            print("Request not successful,Response code ", ticekt_request.status_code, " \nResponse : ",ticekt_request.content)
+            messages.info(request, f' Ticket is Not Created')
         return redirect('submit_form')
